67b2ba87aa01f7853fb6a7531ff4e48cef581ac7
rjblack0@ <rjblack0@live.com>
2026-03-18 16:08:33 -0500

    Module 3 completed baseline

--- travlr/app_server/controllers/travel.js
+++ travlr/app_server/controllers/travel.js
@@ -1,6 +1,9 @@
-/* GET travel view */
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+/* GET Travel page. */
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways'});
+    res.render('travel', { title: 'Travlr Getaways', trips });
 };
 
 module.exports = {
