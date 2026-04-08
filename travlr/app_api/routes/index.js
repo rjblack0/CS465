0d25b08fe2a7eb073da19da60eaf0dbe522c06d3
rjblack0@ <rjblack0@live.com>
2026-04-08 13:35:08 -0500

    Module 6 completed baseline

--- travlr/app_api/routes/index.js
+++ travlr/app_api/routes/index.js
@@ -6,12 +6,14 @@ const tripsController = require("../controllers/trips");
 
 router
     .route('/trips')
-    .get(tripsController.tripsList); // GET Method routes tripList
+    .get(tripsController.tripsList) // GET Method routes tripList
+    .post(tripsController.tripsAddTrip); //POST Method adds a trip
 
 // GET Method routes tripsFindByCode = requires parameter
 
 router
     .route('/trips/:tripCode')
-    .get(tripsController.tripsFindByCode);
+    .get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
